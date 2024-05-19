3dd316b075e5d2736ac2e4c7f79618d01a1e03c0
Maysara <Maysara.Elshewehy@gmail.com>
2024-05-19 06:27:21 +0300

    0.0.2
    just cleanup !
    
    use better repo links.

--- src/code/modules/create.ts
+++ src/code/modules/create.ts
@@ -20,13 +20,18 @@
     {
         try
         {
-            // Checks
-            if(!args.as) throw 'Project type is required, use --as <type>';
+            // Get
+            const _create   : string        = typeof args.create   === 'undefined' ? '' : args.create;
+            const _as       : string        = typeof args.as       === 'undefined' ? '' : args.as;
+
+            // Check
+            if(!_create.length || !_as.length)
+            throw 'Project type is required, use --as <type>';
 
             // Extract the arguments
-            const name                  = args.create;
-            const type                  = args.as;
-            const force                 = typeof args.force === 'undefined' ? false : true;
+            const name  : string            = _create;
+            const type  : string            = _as;
+            const force : boolean           = typeof args.force === 'undefined' ? false : true;
 
             // Clone the project repo
             await CloneRepo({ name, type, force });
@@ -46,29 +51,40 @@
     interface CloneRepoOptions
     {
         name        : string;
-        type        : 'npm' | 'electron';
+        type        : string;
         force       : boolean;
     }
 
-    async function CloneRepo({ name, type, force }: CloneRepoOptions): Promise<string> {
+    async function CloneRepo({ name, type, force }: CloneRepoOptions): Promise<string>
+    {
         const rootPath = path.join(process.cwd(), name);
 
         // Get/Set the repo info
-        const owner : string = 'maysara-elshewehy';
-        const repo  : string = { npm: 'dummy-npm-package', electron: 'dummy-electron-app' }[type];
+        const owner : string = 'xe-es';
+        let   repo  : string = '';
+        {
+            if(type === 'npm') repo = 'nutty-npm-package';
+            else if(type === 'electron') repo = 'nutty-electron-app';
+
+            else throw new Error('Invalid project type');
+        }
 
         // Check if the repo type is valid
         if (!repo) throw new Error('Invalid project type');
+
         // Check if the folder already exists
-        if (fs.existsSync(rootPath)) {
+        if (fs.existsSync(rootPath))
+        {
             if (!force) throw new Error('Folder already exists');
             else fs.rmSync(rootPath, { recursive: true });
         }
 
         // Clone the repo
         const command = `git clone https://github.com/${owner}/${repo}.git ${name}`;
-        await new Promise<void>((resolve, reject) => {
-            exec(command, (error, stdout, stderr) => {
+        await new Promise<void>((resolve, reject) =>
+        {
+            exec(command, (error, stdout, stderr) =>
+            {
                 if (error) reject(error);
                 else resolve();
             });
